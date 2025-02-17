7b2831fd2055ecb01cd013302837dd783b7bf249
Lê Bá Dũng <lebadung178@gmail.com>
2025-02-17 23:41:34 +0700

    add login backend

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  fullName: String,
-  email: String,
-  password: String,
-
+    fullName : String,
+    email: String,
+    password: String,
+    
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", UserSchema);  
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
