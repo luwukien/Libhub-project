083be9db13a55b099e8c07b7cf7b891322facd7f
Lê Bá Dũng <lebadung178@gmail.com>
2025-02-17 22:59:15 +0700

    add user model

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,43 +1,60 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    return(
-      <div className='h-screen bg-black overflow-hidden relative'>
-        <div className="container h-screen flex items-center justify-center px-20 mx-auto">
-          <div className="">
-            <div>
+  const navigate = useNavigate();
 
-            </div>
+  return (
+    <div className="h-screen bg-black overflow-hidden relative flex items-center justify-center">
+      <div className="w-2/4 h-[75vh] bg-white rounded-lg relative p-16 shadow-cyan-200/20">
+        <form onSubmit={() => {}}>
+          <h4 className="text-2xl font-semibold mb-7 text-yellow-500 text-center">
+            Login
+          </h4>
+
+          {/* Input Email */}
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box w-full"
+            />
           </div>
 
-          <div className="w-2/4 h-[75vh] bg-white rounded-lg relative p-16 shadow-cyan-200/20">
-            <form onSubmit={() => {}}>
-              
-              <h4 className="text-2xl font-semibold mb-7 text-yellow-500">Login</h4>
-              
-              <input type="text" placeholder="Email" className="input-box"/>
-              
-              <button type="submit" className="btn-primary text-yellow-500">
-                LOGIN
-              </button>
-
-              <p className="text-yellow-500">Or</p>
-
-              <button
-                type="submit"
-                className="text-yellow-500"
-                onClick={() => {
-                  navigate("/signup");
-                }}
-                >
-                  CREAT ACCOUNT
-                </button>
-
-            </form>
+          {/* Input Password */}
+          <div className="mb-4">
+            <input
+              type="password"
+              placeholder="Password"
+              className="input-box w-full"
+            />
           </div>
-        </div>
+
+          {/* Button Login */}
+          <div className="mb-6">
+            <button type="submit" className="btn-primary w-full text-yellow-500">
+              LOGIN
+            </button>
+          </div>
+
+          <p className="text-yellow-500 text-center mb-4">Or</p>
+
+          {/* Button Create Account */}
+          <div>
+            <button
+              type="button"
+              className="w-full text-yellow-500"
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              CREATE ACCOUNT
+            </button>
+          </div>
+        </form>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
