f04791eb8b783c8be6e985c6a0f0fc4b8e0e9aa0
hieppotato <hieppotato@gmail.com>
2025-02-17 22:49:35 +0700

    add config and user model

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,62 @@
+const config = require("./config.json");
+const mongoose = require("mongoose");
+require("dotenv").config;
+const bcrypt = require("bcrypt");
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+mongoose.connect(config.connectionString);
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-app.get("/hello", async(req, res) => {
-    return res.status(200).json({message: "Hello"});
+const User = require("./models/user.model.js");
+
+//Creat Account
+app.post("/signup", async (req, res) => {
+    const { fullName, email, password } = req.body;
+    if (!fullName || !email || !password) {
+        return res
+            .status(400)
+            .json({ error: true, message: "All fields are required" });
+    }
+
+    const isUser = await User.findOne({ email });
+
+    if (isUser) {
+        return res
+            .status(400)
+            .json({ error: true, message: "User already exits" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        fullName,
+        email,
+        password: hashedPassword,
+    });
+
+    await user.save();
+    const accsessToken = jwt.sign(
+
+        { userId: user._id },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "72h",
+        }
+    );
+
+    return res.status(201).json({
+        error: false,
+        user: { fullName: user.fullName, email: user.email },
+        accsessToken,
+        message: "Registration Successful",
+    });
+
+
 });
 
 app.listen(8000);
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import React from "react";
 
 const Login = () => {
