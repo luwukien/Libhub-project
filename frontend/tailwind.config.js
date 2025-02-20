c665147daf4917a634e9d77687157121741242dc
luwukien <luukien2910@gmail.com>
2025-02-21 05:13:10 +0700

    feat: adding footer; fix: font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,11 +7,16 @@ export default {
   theme: {
     fontFamily: {
       display: ['Kumbh Sans', "Poppins", "sans-serif"],
+      NunitoSans: ['Nunito Sans','san-serif'],
     },
     extend: {
       colors:{
         primary: "#05B6D3",
         secondary: "#EF863E",
+        pornhub: { 
+          200: '#ff9900',
+          300: '#ffbb00',
+        }
       }
     },
     animation: {
