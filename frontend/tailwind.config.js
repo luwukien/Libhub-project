b90f38bed96999738866a9a57ac6c9ae4426ce0d
ChiKien_Luu <luukien2910@gmail.com>
2025-02-21 04:38:44 +0700

    feat: update footer; fix: font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,12 +6,17 @@ export default {
   ],
   theme: {
     fontFamily: {
-      display: ['Kumbh Sans', "Poppins", "sans-serif"],
+      display : ['Kumbh Sans', "Poppins", "sans-serif"],
+      NunitoSans: ['Nunito Sans','san-serif'],
     },
     extend: {
       colors:{
         primary: "#05B6D3",
         secondary: "#EF863E",
+        pornhub: { 
+          200: '#ff9900',
+          300: '#ffbb00',
+        }
       }
     },
     animation: {
