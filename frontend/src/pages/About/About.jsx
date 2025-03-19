40355bb7b0b71a50e560bc810c8fbaada1b7974e
Nguyen Hoang Thao <hoangthaocoder@gmail.com>
2025-03-20 01:45:10 +0700

    simply about us page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Header from '../../components/layouts/Header';
-import { useSearch } from '../../utils/useSearch';
-import Footer from '../../components/layouts/Footer';
+import React from "react";
+import Header from "../../components/layouts/Header";
+import { useSearch } from "../../utils/useSearch";
+import Footer from "../../components/layouts/Footer";
+import "./styles.css";
 const About = () => {
   const {
     userInfo,
@@ -10,7 +11,6 @@ const About = () => {
     onSearchBook,
     handleClearSearch,
   } = useSearch();
-
 
   return (
     <div className="content-wrapper font-NunitoSans">
@@ -24,34 +24,56 @@ const About = () => {
         />
       </header>
 
-      <main className=''>
-        <div className='relative h-[400px]'>
+      <main className=" ">
+        <div className="relative h-[400px]">
           <div
-            className="bg-slate-500 absolute top-0 left-0 w-full h-[400px] bg-center bg-no-repeat bg-cover"
-            style={{ backgroundImage: "url('./public/bg-about-2.jpg')", filter: 'blur(1px)' }}
+            className="absolute flex flex-col items-center justify-center top-0 bottom-0 bg-slate-500 w-full h-[400px] bg-center bg-no-repeat bg-cover"
+            style={{
+              backgroundImage: "url('./public/bg-about-2.jpg')",
+            }}
           >
-          </div>
-          <div className='absolute ct-subheadline top-0 left-0 w-full  h-[400px] bg-gray-100 bg-opacity-50 '>
-            Know us better
+            <h1 className="text-white text-[80px] font-bold">About Us</h1>
+            <h2 className="text-white text-2xl font-bold">
+              For explorers everywhere.
+            </h2>
           </div>
         </div>
-        <div className='container '>
-          <div className='  '>
-            Libhub stands for Library Hub. This is an innovative improvement to the schools library system, designed to enhance students learning experience. It simplifies the search for academic resources, making it easier to find relevant materials. Libhub, simplifies resource searching and provides a virtual library simulation, making it easier for students to access and explore academic materials. We aim to optimize UI/UX to make the library more user-friendly and interesting.
-
-          </div>
-          <div>
-            Thanh vien
+        <div className="wrapper">
+          <div className="pt-[70px] font-NunitoSans text-lg text-[#4B4B4B]">
+            <p className="mt-4">
+              Libhub stands for Library Hub. This is an innovative improvement
+              to the schools library system, designed to enhance students
+              learning experience. It simplifies the search for academic
+              resources, making it easier to find relevant materials.{" "}
+            </p>
+            
+            <p className="mt-4">
+              Libhub, simplifies resource searching and provides a virtual
+              library simulation, making it easier for students to access and
+              explore academic materials. We aim to optimize UI/UX to make the
+              library more user-friendly and interesting.
+            </p>
+            <p className="mt-4">
+              As a reader and at FPT University, we want to create a
+              student-friendly library that helps them enjoy reading. Not only
+              is it a quiet space to study, the library is also a place to
+              inspire, stimulate creativity and connect knowledge-loving souls.
+            </p>
+            <p className="mt-4">
+              We believe that reading not only helps expand knowledge but is
+              also the key to developing critical thinking and creativity.
+            </p>
+            <p className="mt-4">
+              Therefore, our library not only provides thousands of books in
+              many different fields but also makes it easy for them to access
+              the books by indicating their location on the bookshelf.
+            </p>
           </div>
         </div>
       </main>
 
-      <footer>
-        {/* <Footer /> */}
-      </footer>
+      <Footer />
     </div>
   );
-
-
 };
 export default About;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ import axios from 'axios';
 import GameCard from "../../components/Cards/GameCard";
 import CardSlider from "../../components/Cards/CardSlider";
 import { useSearch } from "../../utils/useSearch";  // Import the custom hook
+import "../About/styles.css";
 
 const Home = () => {
   const [items, setItems] = useState({ categories: [], hotBooks: [] });
